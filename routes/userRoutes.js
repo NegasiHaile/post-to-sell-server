2f00758123b1c9911894431b750d2535aeb229bd
unknown <you@example.com>
2022-03-11 16:57:09 +0300

    Added :Route for user profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userCntrlr = require("../controllers/userCntrlr");
+const Auth = require("../middleware/Auth");
 // Signup new user
 router.post("/signup", userCntrlr.signUp);
 
@@ -14,6 +15,9 @@ router.delete("/delete/:id", userCntrlr.deleteUser);
 
 // Signin router
 router.post("/signin", userCntrlr.signIn);
+
+// Get profile
+router.get("/profile", Auth, userCntrlr.getProfile);
 
 // Refresh token
 router.get("/refresh_token", userCntrlr.refreshToken);
