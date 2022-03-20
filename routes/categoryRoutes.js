f52da137b9b2a22c315db761f60ab6e93269dad4
unknown <you@example.com>
2022-03-20 12:13:26 +0300

    Updated :Auth middleware added!

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const categoryCntrlr = require("../controllers/categoryCntrlr");
 const Auth = require("../middleware/Auth");
 
-router.post("/add", categoryCntrlr.addCategory);
+router.post("/add", Auth(["admin"]), categoryCntrlr.addCategory);
 
 router.get("/list", categoryCntrlr.getCategories);
 
-router.put("/edit/:id", categoryCntrlr.editCategory);
+router.put("/edit/:id", Auth(["admin"]), categoryCntrlr.editCategory);
 
-router.delete("/delete/:id", categoryCntrlr.deleteCategory);
+router.delete("/delete/:id", Auth(["admin"]), categoryCntrlr.deleteCategory);
 module.exports = router;
