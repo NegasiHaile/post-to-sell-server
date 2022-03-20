4a6fcd7b3ec18b522677490d60bea742b54bd40a
unknown <you@example.com>
2022-03-20 12:03:03 +0300

    Updated :Category controller validation

--- controllers/categoryCntrlr.js
+++ controllers/categoryCntrlr.js
@@ -5,25 +5,35 @@ const categoryCntrlr = {
   addCategory: async (req, res) => {
     try {
       const { category, description } = req.body;
+      const existingCategory = await Categories.findOne({ category: category });
+      if (category === null)
+        return res.status(400).json({ msg: "Category name is required" });
+
+      if (existingCategory)
+        return res.status(400).json({ msg: "Category exist!" });
       const newCategory = new Categories({
         category,
         description,
       });
-
       await newCategory.save();
-      res.json({ msg: "Category aded successfuly!" });
+      res.json({ msg: "Category added successfuly!" });
     } catch (error) {
       res.status(500).json({ msg: error.message });
     }
   },
   // Fetch alla categories
   getCategories: async (req, res) => {
-    res.json({ msg: await Categories.find() });
+    res.json(await Categories.find());
   },
   // Edit a category
   editCategory: async (req, res) => {
     try {
-      await Categoryies.findOneAndUpdate(
+      const existingCategory = await Categories.findById({
+        _id: req.params.id,
+      });
+      if (!existingCategory)
+        return res.status(400).json({ msg: "Category not found!" });
+      await Categories.findOneAndUpdate(
         { _id: req.params.id },
         ({ category, description } = req.body)
       );
