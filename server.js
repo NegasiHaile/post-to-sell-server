e1e47b5efd68aa6f1b1a929d8854a4be87e40382
unknown <you@example.com>
2022-04-02 12:42:33 +0300

    Updated :banners root url to nanners

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/categories", require("./routes/categoryRoutes"));
 app.use("/api/adverts", require("./routes/advertRoutes"));
-app.use("/api/banner", require("./routes/bannerRouters"));
+app.use("/api/banners", require("./routes/bannerRouters"));
 
 // define the server port
 const PORT = process.env.PORT || 5000;
