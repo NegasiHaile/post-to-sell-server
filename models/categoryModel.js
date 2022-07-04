a433a5f17b475b7b555c01dea6b886b844a3fe02
Negasi <NegasiHaile.Abadi@gmail.com>
2022-07-04 10:07:13 +0300

    Added featuredPostFee field to category

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -15,6 +15,10 @@ const categorySchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    featuredPostFee: {
+      type: Number,
+      required: true,
+    },
     description: {
       type: String,
       trim: true,
